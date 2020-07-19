2201f6bd18f2830d094566020766664dee35709a
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-07-19 16:09:25 +0530

    Adding a getter method which accepts Ids
    If we just add {} that would become a varibale section which I can get it inside a varibale with the annonation of @PathVariable.  However reporting errors like item not found is yet to be done

--- SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicController.java
+++ SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicController.java
@@ -1,6 +1,7 @@
 package org.gitbub.debojyoti.CourseDemoApp.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,16 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
-    @RequestMapping("/getalltopics")
+    // By default it is a mapping for a get request
+    @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topic/{topicID}")
+    public Topic getTopicWithId(@PathVariable("topicID") String id) {
+        // As the return type of the service is Optional may be there is check required to report error
+        // TODO: Add Error handling if optional value is null
+        return topicService.getTopic(id).get();
     }
 }
--- SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicService.java
+++ SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicService.java
@@ -4,17 +4,22 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TopicService {
 
     private List<Topic> topics = Arrays.asList(
             new Topic("1", "Machine Learning", "Has a path for machine learning items"),
-            new Topic("2,", "Windows System Programming", "Windows kernel programming and stuff"),
+            new Topic("2", "Windows System Programming", "Windows kernel programming and stuff"),
             new Topic("3", "Spring Boot", "A definitive resource for all spring related items")
     );
 
     public List<Topic> getAllTopics() {
         return topics;
+    }
+
+    public Optional<Topic> getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst();
     }
 }
