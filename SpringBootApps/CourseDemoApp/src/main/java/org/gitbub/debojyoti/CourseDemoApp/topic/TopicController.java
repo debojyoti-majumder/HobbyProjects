8d9537c98d6c86ed5305ca6f509238be3fa32ecc
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-07-19 15:49:30 +0530

    Adding a service for the controller
    Add an depedncy injection in the controller call with the keyword @Autowired. The @Service should ensure it is a singleton class. Code looks more compact now.

--- SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicController.java
+++ SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicController.java
@@ -1,19 +1,19 @@
 package org.gitbub.debojyoti.CourseDemoApp.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/getalltopics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("1", "Machine Learning", "Has a path for machine learning items"),
-                new Topic("2,", "Windows System Programming", "Windows kernel programming and stuff")
-        );
+        return topicService.getAllTopics();
     }
 }
---
+++ SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicService.java
@@ -0,0 +1,20 @@
+package org.gitbub.debojyoti.CourseDemoApp.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+            new Topic("1", "Machine Learning", "Has a path for machine learning items"),
+            new Topic("2,", "Windows System Programming", "Windows kernel programming and stuff"),
+            new Topic("3", "Spring Boot", "A definitive resource for all spring related items")
+    );
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+}
