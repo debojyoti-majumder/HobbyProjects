d5c81d9e479d5aa07c57494202cf691123e8765c
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-07-19 12:53:09 +0530

    Initial commit for the demo application

---
+++ SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/CourseDemoAppApplication.java
@@ -0,0 +1,13 @@
+package org.gitbub.debojyoti.CourseDemoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDemoAppApplication.class, args);
+	}
+
+}
---
+++ SpringBootApps/CourseDemoApp/src/test/java/org/gitbub/debojyoti/CourseDemoApp/CourseDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gitbub.debojyoti.CourseDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
