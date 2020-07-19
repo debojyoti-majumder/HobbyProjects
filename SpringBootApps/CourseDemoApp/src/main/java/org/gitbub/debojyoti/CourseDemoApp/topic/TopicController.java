e97a3f6a6774d074c930246882eaf6c185e82840
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-07-19 17:41:32 +0530

    Added logging functionality
    Adding spring output ANSI making the output as colored which looks nice. For the loggerer class it seems like it needs to be static and final otherwise the context loading was not working correctly.

--- SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicController.java
+++ SpringBootApps/CourseDemoApp/src/main/java/org/gitbub/debojyoti/CourseDemoApp/topic/TopicController.java
@@ -1,5 +1,8 @@
 package org.gitbub.debojyoti.CourseDemoApp.topic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +13,12 @@ public class TopicController {
 
     @Autowired
     private TopicService topicService;
+    private static final Logger logger = LoggerFactory.getLogger(TopicController.class);
 
     // By default it is a mapping for a get request
     @GetMapping("/topics")
     public List<Topic> getAllTopics() {
+        logger.info("Listing all the topics");
         return topicService.getAllTopics();
     }
 
@@ -21,13 +26,16 @@ public class TopicController {
     public Topic getTopicWithId(@PathVariable("topicID") String id) {
         // As the return type of the service is Optional may be there is check required to report error
         // TODO: Add Error handling if optional value is null
+        logger.info("Getting topic with Topic ID" + id);
         return topicService.getTopic(id).get();
     }
 
     @PostMapping(value = "/topics")
     public void postTopic(@RequestBody Topic newTopic) {
         // TODO: Same as the other GET there is no error handling that have been implemented
+        logger.info("Creating a new topic with ID" + newTopic.getId());
         topicService.add(newTopic);
+
         return;
     }
 }
