81b389b71b5b4c9e083c4033bd82d4a2aa5751d9
Debojyoti Majumder <debojyotim@vmware.com>
2020-10-20 11:52:57 +0530

    Updating the app started using visual studio

---
+++ NetCoreApps/NetCoreApplications/Tutorial1/Pages/QuestionGroups/ListView.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace Tutorial1.Pages.QuestionGroups
+{
+    public class ListViewModel : PageModel
+    {
+        public ListViewModel(IConfiguration configuration)
+        {
+            AppConfig = configuration;
+        }
+
+        public string WelcomeMessage { get; set; }
+        private readonly IConfiguration AppConfig;
+
+        public void OnGet()
+        {
+            WelcomeMessage = AppConfig["TopicsMessage"];
+        }
+    }
+}
