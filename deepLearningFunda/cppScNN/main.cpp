e7d322a1c063b7d12ca1f7f80841d0d903b972d1
Debojyoti Majumder <debojyotim@vmware.com>
2020-10-26 09:50:51 +0530

    Added basic construct function

--- deepLearningFunda/cppScNN/main.cpp
+++ deepLearningFunda/cppScNN/main.cpp
@@ -22,10 +22,45 @@ class NeuronUnit {
 };
 
 class Layer {
+    private:
+        vector<float> _input;
+        vector<float> _output;
 
+    public:
+        Layer(vector<float>& input, int outputLength) {
+
+        }
+
+        Layer(const Layer& prevLayer, int outputLength) {
+
+        }
 };
 
 class NeuralNetwork {
+    private:
+        Layer inputLayer;
+        Layer outputLayer;
+
+        vector<Layer> hiddenLayer;
+
+    public:
+        NeuralNetwork(vector<float> inputValues, size_t outputLength) : 
+            inputLayer(inputValues, outputLength),  
+            outputLayer(inputLayer, outputLength) {
+
+        }
+
+        // TODO: To add actual impelementation
+        bool addHiddenLayer(size_t inputLength, size_t outputLength) {
+            return true;
+        }
+
+        // TODO: Add actual impl
+        vector<float> doForwardPass() {
+            vector<float> returnValue;
+
+            return returnValue;
+        }
 
 };
 
