2b7eb1e390745c18dc14a1776c040547fede0218
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2022-06-25 11:27:53 +0530

    Started using intervals
    This program has a bug, the interval does not seems to be obeying the interval and there is no way to clear the interval object

--- ndjsSnippets/elev.mjs
+++ ndjsSnippets/elev.mjs
@@ -1,5 +1,47 @@
 import readline from 'readline'
 
+class ElevatorScheduler {
+    /**
+     * 
+     * @param {Number} maxNumOfFloors 
+     */
+    constructor(maxNumOfFloors) {
+        this.floorCount = maxNumOfFloors
+
+        // As of now using hard coded elevators
+        this.elevatorCount = 2
+        this.interValObjects = {}
+    }
+
+    routeMonitor() {
+        console.log("Should have a job ID for this")
+    }
+
+    /**
+     * Ideally this function should return a promise then the request has been served
+     * 
+     * @param {Number} source 
+     * @param {Number} destination 
+     */
+    schedule(source, destination) {
+        if( source == destination ) {
+            console.log("Elevator does not require to move")
+            return
+        }
+
+        const upDirection = false;
+
+        if( destination > source ) {
+            upDirection = true
+        }
+
+        const jobIntervalObject = setInterval( () => {
+            this.routeMonitor(),
+            1000
+        });
+    }
+}
+
 class ReplLoop {
     /**
      * 
@@ -17,6 +59,7 @@ class ReplLoop {
         });
 
         this.onLoopEnd = onQuit;
+        this.scheduler = null
     }
 
     /**
@@ -27,7 +70,30 @@ class ReplLoop {
         if( inputString === "help" ) {
             console.log("ELEV 10")
             console.log("SCHED 3 8")
+            return
         }
+
+        let inputStringArr = inputString.split(/(\s+)/);
+        let command = inputStringArr[0];
+
+        if( command === "ELEV" ) {
+            if( this.scheduler == null ) {
+                // The data would be fected later
+                this.scheduler = new ElevatorScheduler(14);
+            }
+        }
+        else if( command == "SCHED" ) {
+            if( this.scheduler != null ) {
+                // This data would be parsed later
+                const source = parseInt(inputStringArr[2])
+                const destination = parseInt(inputStringArr[4])
+
+                console.log(`${source} ---> ${destination}`)
+                this.scheduler.schedule(source,destination)
+            } 
+        }
+
+        console.log(`Command ${inputStringArr[0]}`)
     }
 
     start() {
@@ -37,7 +103,9 @@ class ReplLoop {
         this.readLine.question("# ",(input) => {            
             this.processInput(input);
             
-            // This is a recusrive function call 
+            // This is a recusrive function call, which is added to 
+            // replace a while loop. Using a while loop would not work
+            // in this async scenario
             if( input !== this.loopStop ) {
                 this.start();
             }
