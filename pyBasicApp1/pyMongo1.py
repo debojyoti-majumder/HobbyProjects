97c3e3201fb27736d76c73b7e022124e806d5109
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2022-07-25 20:11:20 +0530

    Basic Search working
    The search needs to be exact, still need to see how to do useful/actual query.

--- pyBasicApp1/pyMongo1.py
+++ pyBasicApp1/pyMongo1.py
@@ -1,13 +1,19 @@
-from typing import Collection, Dict
+# This is very simple console application which will try to make the 
+# basic MongoDB opreations. All the code should be in a seperate files
+# for simplicity I am putting all of them on a single file. In this example
+# We are not using a ORM type library all the querys are hand written
+
+from typing import Dict, List
 from datetime import datetime
 import os
-from gridfs import Database
+from bson import ObjectId
 
 # MongoDB Imports
 from pymongo import MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
 from pymongo.results import InsertOneResult
+from pymongo.cursor import Cursor
 
 # This is the model class that we would be using the
 class TodoItem:
@@ -15,10 +21,16 @@ class TodoItem:
         self.taskCaption : str = caption
         self.taskDescription : str = desc
         self.taskCreationTime : datetime = datetime.now()
+        self.Id : ObjectId = None
 
         if len(desc) != 0 :
             self.taskDescription = desc
     
+    def __init__(self, dbObject : Dict) -> None:
+        self.taskCaption = dbObject["caption"]
+        self.taskDescription = dbObject["description"]
+        self.Id = dbObject["_id"]
+
     def getDict(self) -> Dict:
         returnObject = {
             "caption" : self.taskCaption,
@@ -70,17 +82,36 @@ class TodoRepository:
 
         return insertoneRes.inserted_id
     
-    def findTodoItem(id:str) -> TodoItem:
+    def findTodoItem(self, id:str) -> TodoItem:
         pass
 
-    def deleteTodoItem(id:str) -> bool:
+    def deleteTodoItem(self, id:str) -> bool:
         return False;
     
-    def updateDescription(id:str, desc:str) -> bool:
+    def updateDescription(self, id:str, desc:str) -> bool:
         return False;
     
-    def updateCaption(id:str, caption:str) -> bool:
+    def updateCaption(self, id:str, caption:str) -> bool:
         return False;
+    
+    """
+    Retruns list of tasks by it's name
+    As of now the search query needs to be exactly same 
+    so it does not actually makes a sreach more of a lookup
+    """
+    def findTaskByCaption(self, caption:str) -> List[TodoItem]:
+        retValue:List[TodoItem] = []
+
+        queryObject = {
+            "caption" : caption
+        }
+
+        foundItems:Cursor = self.dbCollection.find(queryObject)
+        for task in foundItems:
+            item = TodoItem(task)
+            retValue.append(item)    
+        
+        return retValue
 
 dbConnection : MongoClient = None
 todoDB : Database = None
@@ -94,6 +125,31 @@ def createTask(todoRepo : TodoRepository) -> None :
     taskID = todoRepo.addTodo(newTask)
     print("Created Task ID:{0}".format(taskID))
 
+def findTask(todoRepo : TodoRepository) -> None:
+    taskString = input("Task Name:")
+    
+    # This logic is there just to show case multiple type of
+    # seaches.
+    if len(taskString) != 0:
+        # This query should provide a list of item
+        taskItems = todoRepo.findTaskByCaption(taskString)
+
+        # This list should have been capped will try to add that up.
+        # By deafult it should show top 10 itmes sorted by it's due date
+        for task in taskItems:
+            print("{0}[{2}]\n\t{1}\n\n".format(
+                task.taskCaption, 
+                task.taskDescription,
+                task.Id)
+            )
+    
+    else:
+        # This should ideally retrun none or single item
+        taskString = input("Task ID")
+        foundItems = todoRepo.findTodoItem(taskString)
+        print(foundItems)
+
+    
 # Function Entry point
 def main():
     print("Running simple MongoDB CRUD Prog")
@@ -128,6 +184,8 @@ def main():
         # not performed then no validations are done
         if option == 1:
             createTask(todoRepo)
+        elif option == 4:
+            findTask(todoRepo)
         elif option == 3:
             taskId = input("Id:")
 
