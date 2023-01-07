18d1d32834c9634b52da88f0e3aad615b6cc492a
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2023-01-07 22:07:09 +0530

    Minor updates
    This program have not run a single time yet.

--- deepLearningFunda/basicNurualNetwork.py
+++ deepLearningFunda/basicNurualNetwork.py
@@ -1,3 +1,9 @@
+# Implementation is reffered from https://github.com/mattm/simple-neural-network/blob/master/neural-network.py
+# As some point it should be able to train on MNIST data set
+
+# The Back Propagation Algorithm
+#   https://www4.rgu.ac.uk/files/chapter3%20-%20bp.pdf
+
 import math
 import random
 
@@ -50,17 +56,43 @@ class Nuron:
 
         return returned_output
 
-class NeronLayer:
+class NuronLayer:
     def __init__(self, num_nurons:int, bias:int = 0) -> None:
         # In a single nuron layer the bias remains the same
         if bias == 0:
             self.bias:float = random.random()
         
+        # Building the layer
         self.nurons:list[Nuron] = []
+
         for _ in range(num_nurons):
             self.nurons.append(Nuron(self.bias))
+    
+    def _geneate_random_values(self, nums:int) -> list[float]:
+        return_value:list[float] = []
+        for _ in range(nums):
+            return_value.append(random.random())
+
+        return return_value
+
+    def init_weights(self, prev_layer_length:int)-> None:
+        for n in self.nurons:
+            random_weights = self._geneate_random_values(prev_layer_length)
+            n.set_weights(random_weights)
+
+# As on now this is harc coded network which has
+# Once input layer, one hidden layer
+class NuralNetwrok:
+    def __init__(self, input_length:int, output_length:int) -> None:
+        self.input_layer:NuronLayer = NuronLayer(input_length)
+        self.output_layer:NuronLayer = NuronLayer(output_length)
+
+        # Hidden network length is now hard coded
+        self.hidden_layer:NuronLayer = NuronLayer(16)
+
+        # Init with random weight
+        self.hidden_layer.init_weights(input_length)
 
 print("A very basic hardwritten Nuralnet in python")
 
-oneNuron = Nuron(0.4)
-oneNuron.set_weights([0.2,0.3,0.4])
+basicNetwrok:NuralNetwrok = NuralNetwrok(4,4)
