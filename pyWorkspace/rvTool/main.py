bd7a3403d0a847a3ccc6823b470199d7000baa42
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2025-08-22 22:34:54 +0530

    Task data can be parsed using post body
    Needed to define a class which would represent the JSON representation of input which in this case is NewTask. And the rest is handled by the framework.

--- pyWorkspace/rvTool/main.py
+++ pyWorkspace/rvTool/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+from models import task
 app = FastAPI()
 
 # Install dependency pip install "fastapi[standard]"
 # To run the application use `fastapi dev main.py`
 # API Endpoint is in http://127.0.0.1:8000
 
+class NewTask(BaseModel):
+    caption:str
+
 @app.get("/")
 async def root():
     return {"message":"Hello FastAPI"}
+
+@app.post("/task")
+async def create_task(task:NewTask):
+    return task
